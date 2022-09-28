2d2bbaecee5e443ec02b201940d03dc659230d8a
505 <505@academicos.cesde.edu.co>
2022-09-28 17:48:58 -0500

    actualizacion v2

--- analisisempleados.py
+++ analisisempleados.py
@@ -5,16 +5,16 @@ tablaEmpleados=pd.read_csv('./empleados.csv')
 #print(tablaEmpleados.to_string())
 
 #filtro 1 quiero obtener todos los datos de los analistas 1
-#tablaAnalistas1=tablaEmpleados[tablaEmpleados["cargo"]=="analista1"].head(50)
-#archivoHTML=tablaAnalistas1.to_html()
-#archivoTEXTO=open("datosanalistas1.html","w")
-#archivoTEXTO.write(archivoHTML)
-#archivoTEXTO.close()
+tablaAnalistas1=tablaEmpleados[tablaEmpleados["cargo"]=="analista1"].head(50)
+archivoHTML=tablaAnalistas1.to_html()
+archivoTEXTO=open("datosanalistas1.html","w",encoding="utf-8")
+archivoTEXTO.write(archivoHTML)
+archivoTEXTO.close()
 
 
 #filtro 2 quiero obtener todos los datos de los analistas 2
 tablaAnalistas2=tablaEmpleados[tablaEmpleados["cargo"]=="analista2"].head(50)
 archivoHTML=tablaAnalistas2.to_html()
-archivoTEXTO=open("datosanalistas2.html","w")
+archivoTEXTO=open("datosanalistas2.html","w",encoding="utf-8")
 archivoTEXTO.write(archivoHTML)
 archivoTEXTO.close()
---
+++ filtro1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+tablaMunicipios=pd.read_csv("./Siembras.csv")
+
+datosBarbosa=tablaMunicipios[tablaMunicipios["Ciudad"]=="Barbosa"]
+print(datosBarbosa)
+archivoHTML=datosBarbosa.to_html()
